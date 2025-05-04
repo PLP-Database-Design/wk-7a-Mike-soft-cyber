4b3d2212a7e79166dd07b7f587c63b46d9a08667
Michael-kariuki <kariukimichaelw@gmail.com>
2025-05-04 21:08:24 +0300

    first changes

--- answers.sql
+++ answers.sql
@@ -1,19 +1,41 @@
--- Question 1
-CREATE TABLE orders(
-orderID INT PRIMARY KEY,
-customerName VARCHAR(100),
-product VARCHAR(100)
+ --  Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
 );
-
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 -- Question 2
-CREATE TABLE orders(
-orderID INT PRIMARY KEY,
-customerName VARCHAR(100)
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
 );
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-CREATE TABLE items(
-orders_id INT,
-product VARCHAR(100),
-quantity INT,
-FOREIGN KEY(orders_id) REFERENCES orders(orderID)
+
+CREATE TABLE Product (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
